4ba50a8f0fbecb5bcc0ac48b07810746dbd67672
m208 <avcvet@yandex.ru>
2022-11-07 00:42:13 +0300

    feat: add Mongo connection data

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -3,10 +3,13 @@ import { PORT } from './constants';
 
 import * as serverService from './services/server.service';
 
+const UserName = process.env.MONGO_USER;
+const Password = process.env.MONGO_PASS;
+const clasterInfo = process.env.MONGO_CLUSTER;
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(`mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp`);
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
